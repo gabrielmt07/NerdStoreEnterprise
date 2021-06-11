9e5e60eb0e2498ca302a4cbd9305869950e63df7
unknown <gabriel_motta07@yahoo.com.br>
2021-06-11 12:06:38 -0300

    refatoracao da startup - nao funcionando

---
+++ src/services/NSE.Identidade.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Identidade.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "NSE.Identidade.API",
+                    Version = "v1",
+                    Description = "Esta API faz parte do curso ASP.NET Core Enterprise Applications, Eduardo Pires - desenvolvedor.io",
+                    Contact = new OpenApiContact() { Name = "Gabriel da Motta", Email = "gabriel@yahoo.com.br" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
