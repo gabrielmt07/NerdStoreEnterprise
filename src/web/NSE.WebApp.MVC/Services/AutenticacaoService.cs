779b23622179fd7bac7e9212a13d456b89e33fdd
unknown <gabriel_motta07@yahoo.com.br>
2021-06-22 19:50:01 -0300

    conversando com apiIdentidade - AutenticacaoService

---
+++ src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -0,0 +1,45 @@
+﻿using NSE.WebApp.MVC.Models;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public class AutenticacaoService : IAutenticacaoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AutenticacaoService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> Login(UsuarioLogin usuarioLogin)
+        {
+            var loginContent = new StringContent(
+                JsonSerializer.Serialize(usuarioLogin),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _httpClient.PostAsync("https://localhost:44396/api/identidade/autenticar", loginContent);
+
+            var teste = await response.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<string>(await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task<string> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            var registroContent = new StringContent(
+                JsonSerializer.Serialize(usuarioRegistro),
+                Encoding.UTF8,
+                "applcation/json");
+
+            var response = await _httpClient.PostAsync("https://localhost:44396/api/identidade/nova-conta", registroContent);
+
+            return JsonSerializer.Deserialize<string>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
--- src/web/NSE.WebApp.MVC/Startup.cs
+++ src/web/NSE.WebApp.MVC/Startup.cs
@@ -20,15 +20,15 @@ namespace NSE.WebApp.MVC
         {
             services.AddIdentityConfiguration();
 
-            services.AddControllersWithViews();
+            services.AddMvcConfiguration();
+
+            services.RegisterServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseMvcConfiguration(env);
-
-            
+            app.UseMvcConfiguration(env);           
         }
     }
 }
