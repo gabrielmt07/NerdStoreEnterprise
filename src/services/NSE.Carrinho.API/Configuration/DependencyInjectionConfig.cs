687c529cfbac4ac96fcd728f8c16301abd139fc1
Gabriel <gabrieltrevizoli@betalearning.com.br>
2021-12-06 09:59:58 -0300

    configuracoes iniciais carrinho

---
+++ src/services/NSE.Carrinho.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.Carrinho.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+
+        }
+    }
+}
