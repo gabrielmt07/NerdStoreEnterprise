de2ad757e2be9682953610f7fae909d77ed4b352
Gabriel <gabrieltrevizoli@betalearning.com.br>
2021-12-07 10:10:17 -0300

    estrutura de dados da api

--- src/services/NSE.Carrinho.API/Configuration/ApiConfig.cs
+++ src/services/NSE.Carrinho.API/Configuration/ApiConfig.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NSE.Carrinho.API.Data;
 using NSE.WebAPI.Core.Identidade;
 
 namespace NSE.Carrinho.API.Configuration
@@ -12,6 +13,8 @@ namespace NSE.Carrinho.API.Configuration
     {
         public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<CarrinhoContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
 
