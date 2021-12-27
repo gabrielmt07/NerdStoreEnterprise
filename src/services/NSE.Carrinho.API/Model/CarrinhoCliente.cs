2fd5cac222a45cd89f9e6ca6e346faf9700ba82a
Gabriel <gabrieltrevizoli@betalearning.com.br>
2021-12-27 09:28:37 -0300

    compostamento de negocio da api - pt I

--- src/services/NSE.Carrinho.API/Model/CarrinhoCliente.cs
+++ src/services/NSE.Carrinho.API/Model/CarrinhoCliente.cs
@@ -7,6 +7,8 @@ namespace NSE.Carrinho.API.Model
 {
     public class CarrinhoCliente
     {
+        internal const int MAX_QUANTIDADE_ITEM = 5;
+
         public Guid Id { get; set; }
         public Guid ClienteId { get; set; }
         public decimal ValorTotal { get; set; }
@@ -19,5 +21,39 @@ namespace NSE.Carrinho.API.Model
         }
 
         public CarrinhoCliente() { }
+
+        internal void CalcularValorCarrinho()
+        {
+            ValorTotal = Itens.Sum(p => p.CalcularValor());
+        }
+
+        internal bool CarrinhoItemExistente(CarrinhoItem item)
+        {
+            return Itens.Any(p => p.ProdutoId == item.ProdutoId);
+        }
+
+        internal CarrinhoItem ObterPorProdutoId(Guid produtoId)
+        {
+            return Itens.FirstOrDefault(p => p.ProdutoId == produtoId);
+        }
+
+        internal void AdicionarItem(CarrinhoItem item)
+        {
+            if (!item.EhValido()) return;
+
+            item.AssociarCarrinho(Id);
+
+            if (CarrinhoItemExistente(item))
+            {
+                var itemExistente = ObterPorProdutoId(item.ProdutoId);
+                itemExistente.AdicionarUnidades(item.Quantidade);
+
+                item = itemExistente;
+                Itens.Remove(itemExistente);
+            }
+
+            Itens.Add(item);
+            CalcularValorCarrinho();
+        }
     }
 }
